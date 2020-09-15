4e775c66af930b3c4ff29a8727cb2a3a6cba98f5
Ednaldo <martinsednaldo@outlook.com>
2020-09-15 17:00:42 -0300

    Evento de submissão para pegar o repositório.

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,25 +1,67 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {FaGithubAlt, FaPlus, FaSpinner} from 'react-icons/fa'
 
-import {FaGithubAlt, FaPlus} from 'react-icons/fa'
 import {Container, Form, SubmitButton} from './style'
+import api from '../../services/api'
 
-export default function Main() {
-  return (
-    <Container>
-      <h1>
-        <FaGithubAlt/>
-        Repositórios
-      </h1>
+export default class Main extends Component {
 
-      <Form onSubmit={() => {}}>
-        <input
-          type="text"
-          placeholder="Adicionar repositório"
-        />
-        <SubmitButton type="submit" disabled>
-          <FaPlus color="#fff" size={14}/>
-        </SubmitButton>
-      </Form>
-    </Container>
-  )
+  state = {
+    newRepository: '',
+    repositories: [],
+    loading: false,
+  }
+
+  handleInputChange = e => {
+    this.setState({newRepository: e.target.value})
+  }
+
+  handleSubmit = async e => {
+    this.setState({ loading: true })
+    e.preventDefault();
+    const {newRepository, repositories} = this.state
+    const res = await api.get(`/repos/${newRepository}`)
+
+    const data = {
+      name: res.data.full_name
+    }
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepository: '',
+      loading: false,
+    })
+  }
+
+  render() {
+
+    const {newRepository, loading} = this.state
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt/>
+          Repositórios
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar repositório"
+            value={newRepository}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton loading={loading}>
+            { loading ?
+                <FaSpinner color="#fff" size={14}/>
+                :
+                <FaPlus color="#fff" size={14}/>
+            }
+          </SubmitButton>
+
+        </Form>
+      </Container>
+    )
+  }
 }
