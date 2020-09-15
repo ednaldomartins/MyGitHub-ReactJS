2546585de6ced04457377d76fe5f4d784733812c
Ednaldo <martinsednaldo@outlook.com>
2020-09-15 14:19:32 -0300

    Hello World.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Routes from './routes';
+import GlobalStyle from './style/global'
+
+function App() {
+  return (
+    <>
+      <Routes/>
+      <GlobalStyle/>
+    </>
+  );
+}
+
+export default App;
