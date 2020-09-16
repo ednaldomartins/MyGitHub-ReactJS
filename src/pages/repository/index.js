8f63c87ef3f26fac3fbf3754ded7c75a09a404a2
Ednaldo <martinsednaldo@outlook.com>
2020-09-16 11:12:32 -0300

    Implementação inicial para a página do repositório.

--- src/pages/repository/index.js
+++ src/pages/repository/index.js
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 
-export default function Repository({match}) {
-  return (
-    <h1>
-      Repository: {decodeURIComponent(match.params.repository)}
-    </h1>
-  )
+import api from '../../services/api'
+import {Loading} from './style'
+
+export default class Repository extends Component {
+
+  state = {
+    repository: {},
+    issues: [],
+    loading: true,
+  }
+
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string
+      })
+    }).isRequired
+  }
+
+  async componentDidMount() {
+    let {match} = this.props
+    let repositoryName = decodeURIComponent(match.params.repository)
+
+    let [repository, issues] = await Promise.all(
+      [
+        api.get(`/repos/${repositoryName}`),
+        api.get(`/repos/${repositoryName}/issues`, {
+          params: {
+            state: 'open',
+            per_page: 5,
+          }
+        })
+      ]
+    )
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false
+    })
+  }
+
+
+  render () {
+    let {repository, issues, loading} = this.state
+
+    if(loading) {
+      return (
+        <Loading>Carregando</Loading>
+      )
+    }
+
+    return (
+      <h1>
+        Repository
+      </h1>
+    )
+  }
 }
